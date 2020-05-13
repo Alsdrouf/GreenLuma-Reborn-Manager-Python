e28c7e5826516beff1f920d9b2640e574fbb56e9
Alsdrouf <alsdroufeichhorn@gmail.com>
2020-05-13 23:43:55 +0200

    imported new functions

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import AddingFile as add
 import Reorder as reorder
 import Option as option
 import Getlist as getlist
+import RemovingFile as remove
 sys.path.append('../')
 
 config = config.ConfigParser()
@@ -23,4 +24,6 @@ print(workpath)
 #numberoffile=len(getlist.Listfile())
 #add.Adding(number=numberoffile, name="Benzema", appid=50)
 #reorder.Reorder()
-getlist.Listgame()
+#getlist.Listgame()
+#remove.Removebyname(name="Benzema")
+#remove.Removebyappid(appid="50")
